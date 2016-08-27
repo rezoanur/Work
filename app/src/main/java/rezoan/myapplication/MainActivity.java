c0e1a8cc67203171a1fbf1139780fa885f653564
rezoanur <rahman.rezoanur@gmail.com>
2016-08-27 11:42:03 +0600

    asd

--- app/src/main/java/rezoan/myapplication/MainActivity.java
+++ app/src/main/java/rezoan/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package rezoan.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,20 +16,38 @@ public class MainActivity extends AppCompatActivity {
 
         Button mybutton=(Button)findViewById(R.id.mybutton);
 
-        assert mybutton != null;
         mybutton.setOnClickListener(
 
-                new Button.OnClickListener(){
+                new OnClickListener(){
 
-                    public void onclick(View v)
-                    {
+                    public void onClick(View v) {
 
-                        TextView mytext=(TextView) findViewById(R.id.mytext);
+                        TextView mytext=(TextView)findViewById(R.id.mytext);
                         mytext.setText("Works");
+
                     }
 
                 }
 
         );
+
+
+        mybutton.setOnLongClickListener(
+
+                new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+
+                        TextView mytext=(TextView)findViewById(R.id.mytext);
+                        mytext.setText("Long pressed!");
+                        return true;
+                    }
+                }
+
+
+        );
+
+
+
     }
 }
