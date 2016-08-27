1cf821cdea7dcdc5c455ac82c0c20149838fb0b6
rezoanur <rahman.rezoanur@gmail.com>
2016-08-27 14:01:54 +0600

    asd

--- app/src/main/java/rezoan/myapplication/MainActivity.java
+++ app/src/main/java/rezoan/myapplication/MainActivity.java
@@ -2,52 +2,95 @@ package rezoan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener{
+
+    private TextView myfield;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button mybutton=(Button)findViewById(R.id.mybutton);
-
-        mybutton.setOnClickListener(
-
-                new OnClickListener(){
-
-                    public void onClick(View v) {
-
-                        TextView mytext=(TextView)findViewById(R.id.mytext);
-                        mytext.setText("Works");
-
-                    }
-
-                }
-
-        );
+        this.myfield =(TextView)findViewById(R.id.myfield);
+        this.gestureDetector= new GestureDetector(this,this);
+        gestureDetector.setOnDoubleTapListener(this);
 
 
-        mybutton.setOnLongClickListener(
+    }
 
-                new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
-                        TextView mytext=(TextView)findViewById(R.id.mytext);
-                        mytext.setText("Long pressed!");
-                        return true;
-                    }
-                }
+        this.gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
 
-        );
+        myfield.setText("SingleTapConfirmed");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+
+        myfield.setText("DoubleTap");
+        return false;
 
 
 
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        myfield.setText("DoubleTapEvent");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+
+        myfield.setText("Down");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+        myfield.setText("DoubleTapEvent");
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+
+        myfield.setText("SingleTapUp");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+        myfield.setText("onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+        myfield.setText("onLongPress");
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        myfield.setText("onFling");
+        return false;
     }
 }
